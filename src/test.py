edcd4a52fa6c88ffcce1a867e55bf7ecff52e199
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-03-17 18:51:37 +0100

    Testing
    Testato codice con grafo grandino.
    
    Co-authored-by: Cristian <CristianCosci@users.noreply.github.com>

--- src/test.py
+++ src/test.py
@@ -10,31 +10,9 @@ if __name__ == "__main__":
     """
         LP3 -> G weight
     """
-    G = nx.gnp_random_graph(1000, .02)
+    G = nx.gnp_random_graph(1000, .01)
 
     print(path_of_length_three.path_of_length_three(G))
 
     nx.draw(G, with_labels=True)
     plt.show()
-    exit(0)
-    # create an empty G
-    G = nx.Graph()
-
-    # add nodes
-    G.add_nodes_from([1, 2, 3, 4])
-
-    # add weighted edges
-    G.add_weighted_edges_from([(1, 2, 0.5), (2, 3, 1.0), (3, 4, 2.0),
-                               (4, 1, 1.5)])
-
-    # access edge weights
-    '''
-        Show weights of G
-
-        print(G[1][2]['weight'])  # print 0.5
-        print(G[3][4]['weight'])  # print 2.0
-    '''
-
-    result = path_of_length_three.path_of_length_three(G)
-
-    print(result)
