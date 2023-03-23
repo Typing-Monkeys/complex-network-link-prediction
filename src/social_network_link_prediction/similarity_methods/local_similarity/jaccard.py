74c690383fe02a9579deacc62a5c01f4eacbdc6d
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-03-23 16:33:48 +0100

    Migliorato codice
    - codice più efficiente
    - rimosso funzione inutile
    
    Co-authored-by: Cristian <CristianCosci@users.noreply.github.com>

--- src/social_network_link_prediction/similarity_methods/local_similarity/jaccard.py
+++ src/social_network_link_prediction/similarity_methods/local_similarity/jaccard.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import numpy as np
-from utils import nodes_to_indexes, get_node_from_index
+from utils import nodes_to_indexes
 from scipy.sparse import lil_matrix, csr_matrix
 from .common_neighbors import __common_neighbors
 
@@ -12,15 +12,15 @@ def __jaccard(G: nx.Graph, x, y) -> float:
 def jaccard(G: nx.Graph) -> csr_matrix:
     size = G.number_of_nodes()
     S = lil_matrix((size, size))
-    name_index_map = nodes_to_indexes(G)
+    name_index_map = list(nodes_to_indexes(G).items())
 
     # itera solo gli indici della matrice traingolare
     # superiore (la matrice è simmetrica)
     # Questo dobrebbe essere molto più veloce di un doppio
     # ciclo for
     for x, y in zip(*np.triu_indices(size)):
-        x_node = get_node_from_index(name_index_map, x)
-        y_node = get_node_from_index(name_index_map, y)
+        x_node = name_index_map[x][0]
+        y_node = name_index_map[y][0]
 
         S[x, y] = __jaccard(G, x_node, y_node)
 
--- src/utils/nodes_to_indexes.py
+++ src/utils/nodes_to_indexes.py
@@ -4,25 +4,3 @@ import networkx as nx
 # TODO: documentazione !!
 def nodes_to_indexes(G: nx.Graph) -> dict:
     return {node_name: index for index, node_name in enumerate(G.nodes)}
-
-
-def get_node_from_index(data: dict, index: int) -> str:
-    """
-        Data una mappatura nodo-indice ritorna
-        il nodo dato un indice.
-
-        ARGS:
-            - data: dizionario con mappatura nodo-indice
-            - index: indice di cui si vuole conoscere il nodo
-
-        RET:
-            - str: nodo mappato con il dato indice
-    """
-
-    # Tutta questa operazione dovrebbe avere costo O(1)
-    # vedi https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary
-
-    key_list = list(data.keys())
-    val_list = list(data.values())
-
-    return key_list[val_list.index(index)]
