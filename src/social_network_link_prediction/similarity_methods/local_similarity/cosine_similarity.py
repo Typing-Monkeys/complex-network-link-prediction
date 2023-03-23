3bf8e522d49f8fd1701904fc9e36263306b80dcc
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-03-23 17:44:25 +0100

    Migliorato codice
    - Migliorata efficienza
    - Testato
    - Sistemati Import
    
    Co-authored-by: Cristian <CristianCosci@users.noreply.github.com>

--- src/social_network_link_prediction/similarity_methods/local_similarity/cosine_similarity.py
+++ src/social_network_link_prediction/similarity_methods/local_similarity/cosine_similarity.py
@@ -1,35 +1,24 @@
 import networkx as nx
 import numpy as np
-
-
-def __common_neighbors(G: nx.Graph, x, y):
-    return len(set(G[x]) & set(G[y]))
+from .common_neighbors import __common_neighbors
+from scipy.sparse import lil_matrix, csr_matrix
+from utils import nodes_to_indexes
 
 
 def __cosine_similarity(G: nx.Graph, x, y) -> float:
     return __common_neighbors(G, x, y) / np.sqrt(G.degree[x] * G.degree[y])
 
 
-def cosine_similarity(G: nx.Graph):
+def cosine_similarity(G: nx.Graph) -> csr_matrix:
     size = G.number_of_nodes()
-    S = np.zeros((size, size))
+    S = lil_matrix((size, size))
+    name_index_map = list(nodes_to_indexes(G).items())
 
-    for x in G:
-        for y in G:
-            S[x, y] = __cosine_similarity(G, x, y)
+    for x, y in zip(*np.triu_indices(size)):
+        x_node = name_index_map[x][0]
+        y_node = name_index_map[y][0]
+           
+        S[x, y] = __cosine_similarity(G, x_node, y_node)
+        S[y, x] = S[x, y]
 
-    return S
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    graph = nx.Graph()
-    graph.add_nodes_from([0, 1, 2, 3])
-    graph.add_edges_from([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
-
-    nx.draw(graph, with_labels=True)
-
-    print(cosine_similarity(graph))
-
-    plt.show()
+    return S.tocsr()
