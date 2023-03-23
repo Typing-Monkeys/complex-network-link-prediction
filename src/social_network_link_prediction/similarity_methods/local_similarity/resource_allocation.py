9581563370a51cdd0e2f7de0473d42b54987f7e7
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-03-23 17:37:15 +0100

    Migliorato codice
    - Migliorata efficienza codice
    - Testato
    - Sistemati import
    
    Co-authored-by: Cristian <CristianCosci@users.noreply.github.com>

--- src/social_network_link_prediction/similarity_methods/local_similarity/resource_allocation.py
+++ src/social_network_link_prediction/similarity_methods/local_similarity/resource_allocation.py
@@ -1,31 +1,23 @@
 import networkx as nx
 import numpy as np
+from utils import nodes_to_indexes
+from scipy.sparse import lil_matrix, csr_matrix
 
 
 def __resource_allocation(G: nx.Graph, x, y) -> float:
     return sum([1 / G.degree[z] for z in set(G[x]) & set(G[y])])
 
 
-def resource_allocation(G: nx.Graph) -> np.ndarray:
+def resource_allocation(G: nx.Graph) -> csr_matrix:
     size = G.number_of_nodes()
-    S = np.zeros((size, size))
+    S = lil_matrix((size, size))
+    name_index_map = list(nodes_to_indexes(G).items())
 
-    for x in G:
-        for y in G:
-            S[x, y] = __resource_allocation(G, x, y)
+    for x, y in zip(*np.triu_indices(size)):
+        x_node = name_index_map[x][0]
+        y_node = name_index_map[y][0]
 
-    return S
+        S[x, y] = __resource_allocation(G, x_node, y_node)
+        S[y, x] = S[x, y]
 
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    graph = nx.Graph()
-    graph.add_nodes_from([0, 1, 2, 3])
-    graph.add_edges_from([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
-
-    nx.draw(graph, with_labels=True)
-
-    print(resource_allocation(graph))
-
-    plt.show()
+    return S.tocsr()
