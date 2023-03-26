7caa50734b9884fb2b313a2cf1cacafd1412538e
Zecown <nicoloposta@gmail.com>
2023-03-26 17:45:53 +0200

    Aggiornata funzione per calcolare SVD
    Aggiunto l'utilizzo delle matrici sparse, da ricontrollare la matematica per essere certi che vada bene come calcola la link prediction

--- src/social_network_link_prediction/dimensionality_reduction_methods/svd.py
+++ src/social_network_link_prediction/dimensionality_reduction_methods/svd.py
@@ -1,42 +1,22 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import svds
 
-def singular_value_decomposition(G, k = 5):
-    # k is the number of singular vectors to use for link prediction
-    # Compute the similarity matrix from the graph
-    A = nx.to_numpy_array(G)
-    S = np.dot(A, A.T)
+def link_prediction_svd(G: nx.Graph, k = 5) -> csr_matrix:
 
-    # Perform singular value decomposition on the similarity matrix
-    U, sigma, VT = np.linalg.svd(S, full_matrices=False)
-
-    # Compute the reduced SVD
-    Uk = U[:, :k]
-    Vk = VT[:k, :]
-    Sk = np.diag(sigma[:k])
-
-    # Compute the predicted similarity matrix
-    S_pred = np.dot(Uk, np.dot(Sk, Vk))
-
-    # Get the edges that are missing from the graph
-    missing_edges = []
-    for u, v in nx.non_edges(G):
-        missing_edges.append((u, v))
-
-    # Compute the predicted scores for the missing edges
-    scores = {}
-    for u, v in missing_edges:
-        score = S_pred[u, v]
-        scores[(u, v)] = score
-
-    # Sort the predicted scores in descending order
-    sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-
-    # Print the top 10 predicted edges
-    print("Top 10 predicted edges:")
-    for edge, score in sorted_scores[:10]:
-        print(edge, score)
+    # Create the adjacency matrix of the graph
+    adj_matrix = nx.adjacency_matrix(G)
+    
+    # Perform the singular value decomposition
+    U, S, Vt = svds(csr_matrix(adj_matrix).astype(float), k=k)
+    S = np.diag(S)
+    
+    # Compute the predicted adj_matrix
+    predicted_adj_matrix = U.dot(S).dot(Vt)
+    
+    return predicted_adj_matrix
 
 if __name__ == "__main__":
     # Load a test graph
@@ -46,6 +26,31 @@ if __name__ == "__main__":
     nx.draw(graph, with_labels=True)
 
     # Compute the SVD of the similarity matrix and do link prediction on it
-    singular_value_decomposition(graph)
+    predicted_adj_matrix = link_prediction_svd(graph)
+
+    # adj_matrix of the starting graph
+    adj_matrix = nx.adjacency_matrix(graph)
+
+    # Get the predicted eges
+    predicted_edges = []
+    for u in range(adj_matrix.shape[0]):
+        for v in range(u+1, adj_matrix.shape[1]):
+            if graph.has_edge(u, v):
+                continue
+            w = predicted_adj_matrix[u, v]
+            predicted_edges.append((u, v, w))
+            
+    # Normalize the predicted edge weights to the range [0, 1]
+    max_weight = max(predicted_edges, key=lambda x: x[2])[2]
+    for i in range(len(predicted_edges)):
+        predicted_edges[i] = (predicted_edges[i][0], predicted_edges[i][1], predicted_edges[i][2]/max_weight)
+    
+    # Sort the predicted edges by weight in descending order
+    predicted_edges.sort(key=lambda x: x[2], reverse=True)
+    
+    # Print the predicted edges with their probability score
+    print("Top 10 Predicted edges:")
+    for edge in predicted_edges[:10]:
+        print("({0}, {1}): {2}".format(edge[0], edge[1], round(edge[2], 3)))
 
     plt.show()
