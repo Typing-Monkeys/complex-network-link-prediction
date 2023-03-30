fce380dea2af133d123664f45e038d71b2e519ab
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-03-30 18:35:59 +0200

    Miglioramento Tests - Local Similarity
    Iniziato miglioramento tests:
    - Aggiunti 3 dataset: easy, medium, hard (efficienza nel caricamento)
    - Aggiunta funzione per eseguire test con dati di scikitnetwork
    - migliorati common neighobrs e jaccard
    - riorganizzati import

--- tests/similarity_methods/test_local.py
+++ tests/similarity_methods/test_local.py
@@ -1,48 +1,107 @@
-import unittest
-from social_network_link_prediction.similarity_methods import local_similarity
 import networkx as nx
-from sknetwork.data import house
+import unittest
 import random
+from social_network_link_prediction.similarity_methods import local_similarity
+from sknetwork.data import house, karate_club, load_konect
+from sknetwork.linkpred import CommonNeighbors
+from sknetwork.linkpred import JaccardIndex
 
 
 class TestLocalSimilarityMethods(unittest.TestCase):
+    __dataset_easy = house()
+    __dataset_medium = karate_club()
+    __dataset_hard = load_konect('ego-facebook', verbose=False)["adjacency"]
 
-    def test_common_neighbors(self):
-        from sknetwork.linkpred import CommonNeighbors
+    def __load_easy_dataset(self):
+        adj = self.__dataset_easy
+        g = nx.from_numpy_array(adj)
 
-        adjacency = house()
+        return g, adj
+
+    def __load_medium_dataset(self):
+        adj = self.__dataset_medium
+        g = nx.from_numpy_array(adj)
+
+        return g, adj
+
+    def __load_hard_dataset(self):
+        adj = self.__dataset_hard
+        g = nx.from_numpy_array(adj)
+
+        return g, adj
+
+    def __perform_sktest(self,
+                         our_values,
+                         skfunction,
+                         samples_range,
+                         samples_number=20):
+        indxes = [(random.randrange(samples_range),
+                   random.randrange(samples_range))
+                  for a in range(samples_number)]
+
+        for i, j in indxes:
+            expected = skfunction.predict((i, j))
+            our_res = our_values[i, j]
+
+            self.assertEqual(expected.round(2), our_res.round(2))
+
+    def test_common_neighbors_1(self):
+        g, adjacency = self.__load_easy_dataset()
         cn = CommonNeighbors()
 
         cn.fit(adjacency)
-        our_sim = local_similarity.common_neighbors(
-            nx.from_numpy_array(adjacency))
+        our_sim = local_similarity.common_neighbors(g)
 
-        indxes = [(random.randrange(adjacency.shape[0]),
-                   random.randrange(adjacency.shape[0])) for a in range(20)]
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0], 20)
 
-        for i, j in indxes:
-            expected = cn.predict((i, j))
-            our_res = our_sim[i, j]
+    def test_common_neighbors_2(self):
+        g, adjacency = self.__load_medium_dataset()
 
-            self.assertEqual(expected, our_res)
+        cn = CommonNeighbors()
+        cn.fit(adjacency)
 
-    def test_jaccard(self):
-        from sknetwork.linkpred import JaccardIndex
+        our_sim = local_similarity.common_neighbors(g)
 
-        adjacency = house()
-        cn = JaccardIndex()
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0])
+
+    def test_common_neighbors_3(self):
+        g, adjacency = self.__load_hard_dataset()
+        cn = CommonNeighbors()
 
         cn.fit(adjacency)
-        our_sim = local_similarity.jaccard(nx.from_numpy_array(adjacency))
+        our_sim = local_similarity.common_neighbors(g)
 
-        indxes = [(random.randrange(adjacency.shape[0]),
-                   random.randrange(adjacency.shape[0])) for a in range(20)]
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0])
 
-        for i, j in indxes:
-            expected = cn.predict((i, j))
-            our_res = our_sim[i, j]
+    def test_jaccard_1(self):
+        g, adjacency = self.__load_easy_dataset()
 
-            self.assertEqual(expected.round(2), our_res.round(2))
+        cn = JaccardIndex()
+        cn.fit(adjacency)
+
+        our_sim = local_similarity.jaccard(g)
+
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0])
+
+    def test_jaccard_2(self):
+        g, adjacency = self.__load_medium_dataset()
+
+        cn = JaccardIndex()
+        cn.fit(adjacency)
+
+        our_sim = local_similarity.jaccard(g)
+
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0])
+
+    def test_jaccard_3(self):
+        g, adjacency = self.__load_hard_dataset()
+
+        cn = JaccardIndex()
+        cn.fit(adjacency)
+
+        our_sim = local_similarity.jaccard(g)
+
+        self.__perform_sktest(our_sim, cn, adjacency.shape[0])
 
     def test_sorensen(self):
         from sknetwork.linkpred import SorensenIndex
