9d34c8501ccff7144b4fd3cc0f3d14c2813d2328
CristianCosci <cristiancosci@gmail.com>
2023-04-01 16:10:26 +0200

    Implementato shortest path

---
+++ src/social_network_link_prediction/similarity_methods/global_similarity/shortest_path.py
@@ -0,0 +1,17 @@
+import networkx as nx
+from scipy.sparse import csr_matrix, lil_matrix
+
+def shortest_path(G: nx.Graph, cutoff: int = None) -> csr_matrix:
+    dim = len(G.nodes)
+    if cutoff is None:
+        cutoff = dim
+
+    lengths = dict(nx.all_pairs_shortest_path_length(G, cutoff))
+    
+    S = lil_matrix((dim, dim))
+    for source_node in lengths.keys():
+        for dest_node in lengths[source_node].keys():
+            S[source_node, dest_node] = -lengths[source_node][dest_node]
+
+    #andrebbero normalizzati i valori per riportarli tra 0 e 1? o lasciamo i negativi?
+    return S.tocsr()
