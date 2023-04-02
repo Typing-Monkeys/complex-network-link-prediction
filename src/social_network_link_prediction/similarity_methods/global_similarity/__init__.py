f2d8913607b76d53b0bf87af7fcae53b896f8d46
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-04-02 17:38:51 +0200

    Global Sim Docs
    Aggiunta documentazione per i metodi di Global Similarity.

--- src/social_network_link_prediction/similarity_methods/global_similarity/__init__.py
+++ src/social_network_link_prediction/similarity_methods/global_similarity/__init__.py
@@ -1,1 +1,8 @@
+"""Collection of Global Similarity Methods for Link Prediction.
+
+Global indices are computed using entire
+topological information of a network.
+The computational complexities of such methods
+are higher and seem to be infeasible for large networks.
+"""
 from .katz_index import katz_index
--- src/social_network_link_prediction/similarity_methods/global_similarity/katz_index.py
+++ src/social_network_link_prediction/similarity_methods/global_similarity/katz_index.py
@@ -8,6 +8,7 @@ def __power_method(A: csr_matrix,
                    max_iterations: int = 100,
                    tol: float = 1e-12,
                    verbose: bool = False):
+    """Perfome the Power Method"""
     n = A.shape[0]
     x = np.ones(n) / np.sqrt(n)  # initialize a vector x
     # r = A @ x - np.dot(A @ x, x) * x # residual initialization
@@ -37,19 +38,40 @@ def __power_method(A: csr_matrix,
 
 
 def katz_index(G: nx.Graph, beta: int = 1) -> csr_matrix:
-    """TODO
+    """Compute the Katz Index for all nodes in the Graph.
+    Each similarity value is defined as:
+
+    .. math::
+        S(x, y) = (I - \\beta A)^{-1} - I
+
+    where \\(I\\) is the Identity Matrix,
+    \\(\\beta\\) is a dumping factor that controls the path weights
+    and \\(A\\) is the Adjacency Matrix
 
     Parameters
     ----------
     G: nx.Graph :
-        grafo da analizzare
+        input Graph (a networkx Graph)
     beta: int :
-        TODO
+        Dumping Factor
          (Default value = 1)
 
     Returns
     -------
-    S: csr_matrix : matrice di Similarità
+    S: csr_matrix : the Similarity Matrix (in sparse format)
+
+    Notes
+    -----
+    For the convergence of above equation,
+
+    .. math::
+        \\beta < \\frac{1}{\\lambda_1}
+
+    where \\(\\lambda_1\\) is the maximum eighenvalue of the matrix \\(A\\).
+
+    The computational complexity of the given metric is high,
+    and it can be roughly estimated to be cubic complexity
+    which is not feasible for a large network.
     """
     A = to_adjacency_matrix(G)
     largest_eigenvalue = __power_method(A)  # lambda_1
