5b67cebcba4ad282ac3f61c00d2910638ddfb5df
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-04-02 18:05:40 +0200

    Quasi-local Docs
    Aggiunta documentazione per i metodi Quasi-local.

--- src/social_network_link_prediction/similarity_methods/quasi_local_similarity/path_of_length_three.py
+++ src/social_network_link_prediction/similarity_methods/quasi_local_similarity/path_of_length_three.py
@@ -5,6 +5,7 @@ from scipy.sparse import lil_matrix, csr_matrix
 
 
 def __path_of_length_three_iter(G: nx.Graph, x, y) -> float:
+    """Compute the Path of Length Three Index for 2 given nodes."""
     k_x = G.degree(x)
     k_y = G.degree(y)
 
@@ -31,7 +32,13 @@ def __path_of_length_three_iter(G: nx.Graph, x, y) -> float:
 
 
 def path_of_length_three(G: nx.Graph) -> csr_matrix:
-    """TODO
+    """Compute the Path of Length Three Index for all nodes in the Graph.
+    Each similarity value is defined as:
+
+    .. math::
+        S(x, y) = \\sum \\frac{a_{x,u}a_{u,v}a_{v,y}}{k_u k_v}
+
+    where \\(k_x\\) is the degree of node \\(x\\).
 
     Parameters
     ----------
@@ -41,6 +48,21 @@ def path_of_length_three(G: nx.Graph) -> csr_matrix:
     Returns
     -------
     S: csr_matrix : matrice di Similarità
+
+    Notes
+    -----
+    Georg Simmel, a German sociologist, first coined the concept
+    “triadic closure” and made popular by Mark Granovetter in his work
+    “The Strength of Weak Ties”. The authors proposed a similarity index
+    in protein-protein interaction (PPI) network,
+    called path of length 3 (or L3) published in the
+    Nature Communication. They experimentally show that the triadic closure
+    principle (TCP) does not work well with PPI networks. They showed the
+    paradoxical behavior of the TCP (i.e., the path of length 2),
+    which does not follow the structural and evolutionary mechanism that
+    governs protein interaction. The TCP predicts well to the interaction of
+    self-interaction proteins (SIPs), which are very small (4%) in PPI networks
+    and fails in prediction between SIP and non SIP that amounts to 96%.
     """
     size = G.number_of_nodes()
     S = lil_matrix((size, size))
