78d7e6bd22d8d29b81d7fb23d8537e29e5b1bb7b
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-04-02 18:34:37 +0200

    Utils Docs
    Aggiunta documentazione ad un metodo nel modulo utils.

--- src/social_network_link_prediction/utils/to_adjacency_matrix.py
+++ src/social_network_link_prediction/utils/to_adjacency_matrix.py
@@ -5,18 +5,19 @@ from scipy.sparse import csc_matrix
 
 def to_adjacency_matrix(G: nx.Graph,
                         sparse: bool = True) -> csc_matrix | np.ndarray:
-    """Dato un grafo ritorna la relativa Matrice di Adiacenza
+    """Convert a ginven Graph in to its Adjacency Matrix
 
     Parameters
     ----------
     G: nx.Graph :
-        grafo di cui ottenere la matrice di adiacenza
+        input Graph (a networkx Graph)
     sparse: bool:
-        Indica se ritornare una Sparse Matrix (True) o Full Matrix (False)
+        if True, return the Adjacency Matrix in sparse format,
+        otherwise in full format.
          (Default value = True)
 
     Returns
     -------
-    sparse | np.ndarray: matrice di adiacenza
+    csc_matrix | np.ndarray: the Adjacency Matrix
     """
     return nx.adjacency_matrix(G) if sparse else nx.to_numpy_array(G)
