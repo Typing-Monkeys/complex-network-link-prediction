f66129c9011211015ee7b484e8e2428082757d9b
CristianCosci <cristiancosci@gmail.com>
2023-04-03 12:39:16 +0200

    Corretto mappatura shortest path

--- src/social_network_link_prediction/similarity_methods/global_similarity/shortest_path.py
+++ src/social_network_link_prediction/similarity_methods/global_similarity/shortest_path.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from scipy.sparse import csr_matrix, lil_matrix
+from social_network_link_prediction.utils import nodes_to_indexes
 
 def shortest_path(G: nx.Graph, cutoff: int = None) -> csr_matrix:
     dim = len(G.nodes)
@@ -7,11 +8,11 @@ def shortest_path(G: nx.Graph, cutoff: int = None) -> csr_matrix:
         cutoff = dim
 
     lengths = dict(nx.all_pairs_shortest_path_length(G, cutoff))
-    
+    nodes_to_indexes_map = nodes_to_indexes(G)
+
     S = lil_matrix((dim, dim))
     for source_node in lengths.keys():
         for dest_node in lengths[source_node].keys():
-            S[source_node, dest_node] = -lengths[source_node][dest_node]
+            S[nodes_to_indexes_map[source_node], nodes_to_indexes_map[dest_node]] = -lengths[source_node][dest_node]
 
-    #andrebbero normalizzati i valori per riportarli tra 0 e 1? o lasciamo i negativi?
     return S.tocsr()
