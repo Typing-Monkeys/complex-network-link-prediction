06af81a8e96c3862a0954f51994d278bbb492e71
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2023-04-07 12:55:34 +0200

    Inserito metodo top 5 element: - effttuati test su karate e grafo piccolo

--- src/test.py
+++ src/test.py
@@ -1,16 +1,28 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 from social_network_link_prediction.similarity_methods.local_similarity import common_neighbors, adamic_adar, preferential_attachment, resource_allocation, cosine_similarity, sorensen, hub_promoted, hub_depressed, node_clustering, jaccard
 from social_network_link_prediction.similarity_methods.quasi_local_similarity import local_path_index, path_of_length_three
 from social_network_link_prediction.similarity_methods.global_similarity import shortest_path, katz_index, rooted_page_rank
 from social_network_link_prediction.dimensionality_reduction_methods import link_prediction_svd, link_prediction_nmf
 
 
-# G =  nx.karate_club_graph()
-G = nx.Graph() 
-G.add_nodes_from([0, 1, 2, 3])
-G.add_edges_from([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
+G =  nx.karate_club_graph()
+# G = nx.Graph() 
+# G.add_nodes_from([0, 1, 2, 3, 4, 5])
+# G.add_edges_from([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (2, 4), (2, 5)])
 
+
+def top_five_element(result):
+    # Get the indices that would sort the array in descending order
+    sorted_indices = np.argsort(result)[::-1]
+
+    # Extract the top 5 elements with the highest values
+    top_5_elements = result[sorted_indices[:5]]
+
+    print("Top 5 elements with the highest values:\n", top_5_elements)
+    print('\n')
+    
 
 def test_LPI():
     nx.draw(G, with_labels=True)
@@ -35,24 +47,31 @@ def test_path_of_length():
 def test_common_neighbors_easy():
 
     nx.draw(G, with_labels=True)
-    print(common_neighbors(G))
-    print('\n')
+    
+    result = common_neighbors(G)
+    
+    top_five_element(result)
+    
     plt.show()
 
-def test_common_neighbors_hard():
-    print(common_neighbors(G))
-    print('\n')
 
+def test_common_neighbors_hard():
     nx.draw(G, with_labels=True)
+    
+    result = common_neighbors(G)
+    
+    top_five_element(result)
+    
     plt.show()
 
 
 # --- Jaccard Measure
 def test_jaccard():
-    print(jaccard(G))
-    print('\n')
-
     nx.draw(G, with_labels=True)
+    result = jaccard(G)
+    
+    top_five_element(result)
+
     plt.show()
 
 
@@ -69,8 +88,10 @@ def test_adamic_adar():
 def test_preferential():
     G = nx.gnp_random_graph(1000, .01)
 
-    print(preferential_attachment(G))
+    result = preferential_attachment(G)
 
+    top_five_element(result)
+    
     nx.draw(G, with_labels=True)
     plt.show()
 
@@ -79,7 +100,9 @@ def test_preferential():
 def test_resourceallocation():
     G = nx.gnp_random_graph(1000, .01)
 
-    print(resource_allocation(G))
+    result = resource_allocation()
+    
+    top_five_element(result)
 
     nx.draw(G, with_labels=True)
     plt.show()
@@ -87,8 +110,9 @@ def test_resourceallocation():
 
 # --- Cosine Similarity
 def test_cosinesimilarity():
-    print(cosine_similarity(G))
-    print('\n')
+    result = cosine_similarity()
+    
+    top_five_element(result)
 
     nx.draw(G, with_labels=True)
     plt.show()
@@ -96,76 +120,76 @@ def test_cosinesimilarity():
 
 # --- Sorensen
 def test_sorensen():
-    print(sorensen(G))
-    print('\n')
-
+    result = sorensen(G)
+    
+    top_five_element(result)
+    
     nx.draw(G, with_labels=True)
     plt.show()
 
 
 # --- Hub Promoted
 def test_hubpromoted():
-    print(hub_promoted(G))
-    print('\n')
+    result = hub_promoted(G)
+    
+    top_five_element(result)
 
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- Hub Depressed
 def test_hubdepressed():
-    print(hub_depressed(G))
-    print('\n')
+    result = hub_depressed(G)
+    
+    top_five_element(result)
     
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- Node Clustering
 def test_nodeclusterintg():
-    G = nx.gnp_random_graph(1000, .01)
-
-    print(node_clustering(G))
+    result = node_clustering(G)
+    top_five_element(result)
 
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- Katz Index
 def test_katz_index():
-    print(katz_index(G, 10))
-    print('\n')
+    result = katz_index(G, 10)
+    
+    top_five_element(result)
 
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- Shortest Path
 def test_shortest_path():
-    print(shortest_path(G, 10))
-    print('\n')
+    result = shortest_path(G, 10)
+    top_five_element(result)
     
     nx.draw(G, with_labels=True)
     plt.show()
     
 # --- Rooted Page Rank
 def test_rooted_page_rank():
-    print(rooted_page_rank(G))
-    print('\n')
+    result = rooted_page_rank(G)
+    top_five_element(result)
     
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- SVD
 def test_svd():
-    G = nx.gnp_random_graph(1000, .01)
-
-    print(link_prediction_svd(G, normalize=True))
-
+    result = link_prediction_svd(G, normalize=True)
+    top_five_element(result)
     nx.draw(G, with_labels=True)
     plt.show()
 
 # --- NMF
 def test_link_prediction_nmf():
-    G = nx.gnp_random_graph(1000, .01)
-    
-    print(link_prediction_nmf(G))
+    result = link_prediction_nmf(G)
+    top_five_element(result)
 
 
 #-----------------------------
@@ -213,8 +237,20 @@ test_adamic_adar()
 test_cosinesimilarity()
 '''
 #---Result
-
-
+#---OK
+'''
+test_common_neighbors_easy()
+test_common_neighbors_hard()
+test_sorensen()
+test_jaccard()
+test_hubdepressed()
+test_hubpromoted()
+test_adamic_adar()
+test_cosinesimilarity()
+test_nodeclusterintg()
+'''
+#---Result
+#-----------------------------
 
 #-----------------------------
 '''
@@ -227,12 +263,13 @@ test_shortest_path()
 '''
 #---Result
 
-#---Ricontrollare 
+#---OK 
 '''
 test_katz_index()
 test_shortest_path()
 test_rooted_page_rank()
 '''
+#---Result
 #-----------------------------
 
 
@@ -247,5 +284,18 @@ test_LPI()
 test_path_of_length()
 '''
 #---Result
+#-----------------------------
 
 
+
+#-----------------------------
+'''
+    Probabilistic index
+'''
+#---
+'''
+test_svd()
+test_link_prediction_nmf()
+'''
+#---Result
+#-----------------------------
