93897a708b31d3d01f6269b7c55412cdb52cf91d
Nicolò Vescera <nicolo.vescera@gmail.com>
2023-04-27 17:08:52 +0200

    fix(src): Riscrittura moduli
    Eliminata cartella inutile e sostituita con file python

--- cnlp/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Some utilities functions."""
-from cnlp.utils.nodes_to_indexes import nodes_to_indexes
-from cnlp.utils.to_adjacency_matrix import to_adjacency_matrix
-from cnlp.utils.only_unconnected import only_unconnected
